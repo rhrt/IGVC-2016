b66efa880e8175dbb3ccb5f4c21743068b13c187
Lewis Kelley <kelleyld@rose-hulman.edu>
2016-01-18 17:38:11 -0500

    Got driving to work.
    Also added driving.launch to the repo.

--- src/motor_controller_test/scripts/joyDrive.py
+++ src/motor_controller_test/scripts/joyDrive.py
@@ -8,22 +8,21 @@ from geometry_msgs.msg import Twist
 from std_msgs.msg import String
 
 def velCallback(cmd_vel):
-    print "In velCallback-----------------------"
-    #TODO Check which wheel is connected to which port
-    #TODO Check the constant to make sure it's reasonable
-    CONVERSION = 12000 # Conversion factor from the -1...1 input to the total velocity
-    netVel = (float(cmd_vel.linear.x)**2 + float(cmd_vel.linear.y)**2)**0.5
+    CONVERSION = 60000 # Conversion factor from the -1...1 input to the total velocity
+    ANGLE_DIM = 0.1 # Conversion factor to make the twists solwer
+    netVel = (cmd_vel.linear.x**2 + cmd_vel.linear.y**2)**0.5
     angle = atan2(cmd_vel.linear.y, cmd_vel.linear.x)
-    print "netVel " + str(netVel)
-    print "angle " + str(angle)
-    print "cmd_vel " + str(cmd_vel)
 
-    roboclaw.SpeedM1(FLeftAddress, int(CONVERSION * (netVel * cos(angle + pi/4) - cmd_vel.angular.z)))
-    roboclaw.SpeedM1(FRightAddress, int(CONVERSION * (netVel * sin(angle + pi/4) + cmd_vel.angular.z)))
-    roboclaw.SpeedM1(BLeftAddress, int(CONVERSION * (netVel * sin(angle + pi/4) - cmd_vel.angular.z)))
-    roboclaw.SpeedM1(BRightAddress, int(CONVERSION * (netVel * cos(angle + pi/4) + cmd_vel.angular.z)))
+    roboclaw.Open("/dev/ttyACM0", 115200)
+    roboclaw.SpeedM1(FLeftAddress, int(-1 * CONVERSION * (netVel * cos(angle + pi/4) - cmd_vel.angular.z * ANGLE_DIM)))
+    roboclaw.Open("/dev/ttyACM1", 115200)
+    roboclaw.SpeedM1(FRightAddress, int(CONVERSION * (netVel * sin(angle + pi/4) + cmd_vel.angular.z * ANGLE_DIM)))
+    roboclaw.Open("/dev/ttyACM2", 115200)
+    roboclaw.SpeedM1(BRightAddress, int(CONVERSION * (netVel * cos(angle + pi/4) + cmd_vel.angular.z * ANGLE_DIM)))
+    roboclaw.SpeedM2(BLeftAddress, int(-1 * CONVERSION * (netVel * sin(angle + pi/4) - cmd_vel.angular.z * ANGLE_DIM)))
 
     # Print speeds of motors
+    rospy.loginfo("-----------------------------------")
     rospy.loginfo("Front right " + str(int(CONVERSION * (netVel * sin(angle + pi/4) + cmd_vel.angular.z))))
     rospy.loginfo("Front left " + str(int(CONVERSION * (netVel * cos(angle + pi/4) - cmd_vel.angular.z))))
     rospy.loginfo("Back right " + str(int(CONVERSION * (netVel * cos(angle + pi/4) + cmd_vel.angular.z))))
@@ -39,43 +38,15 @@ def joyCallback(joy):
     roboclawBack.SpeedM2(address, int(12000*joyY))
 
 def joyDrive():
-    #TODO Check that this works for running two motor controllers
-
-    roboclaw.Open("/dev/ttyACM0", 115200)
-
     global FLeftAddress
     global FRightAddress
     global BLeftAddress
     global BRightAddress
 
-    FLeftAddress = 0x80 # note that this assumes that the frontleft controller is in mode 7,
+    FLeftAddress = 0x80   # note that this assumes that the frontleft controller is in mode 7,
     FRightAddress = 0x81  # and the the frontright is in mode 8
     BLeftAddress = 0x82
     BRightAddress = 0x83
-
-    version = roboclaw.ReadVersion(FLeftAddress)
-    if version[0] == False:
-        print "GETVERSION Failed for Front Left"
-    else:
-        print "Front Left: " + repr(version[1])
-
-    version = roboclaw.ReadVersion(FRightAddress)
-    if version[0] == False:
-        print "GETVERSION Failed for Front Right"
-    else:
-        print "Front Right: " + repr(version[1])
-
-    version = roboclaw.ReadVersion(BLeftAddress)
-    if version[0] == False:
-        print "GETVERSION Failed for Back Left"
-    else:
-        print "Front Left: " + repr(version[1])
-
-    version = roboclaw.ReadVersion(BRightAddress)
-    if version[0] == False:
-        print "GETVERSION Failed for Back Right"
-    else:
-        print "Back Right: " + repr(version[1])
 
     #PID coefficients
     Kp = 35.0
@@ -84,10 +55,13 @@ def joyDrive():
     qpps = 44000
 
     #Set PID Coefficients
+    roboclaw.Open("/dev/ttyACM0", 115200)
     roboclaw.SetM1VelocityPID(FLeftAddress,Kp,Kd,Ki,qpps)
-    # roboclaw.SetM1VelocityPID(FRightAddress,Kp,Kd,Ki,qpps)
-    # roboclaw.SetM1VelocityPID(BLeftAddress,Kp,Kd,Ki,qpps)
-    # roboclaw.SetM1VelocityPID(BRightAddress,Kp,Kd,Ki,qpps)
+    roboclaw.Open("/dev/ttyACM1", 115200)
+    roboclaw.SetM1VelocityPID(FRightAddress,Kp,Kd,Ki,qpps)
+    roboclaw.Open("/dev/ttyACM2", 115200)
+    roboclaw.SetM1VelocityPID(BLeftAddress,Kp,Kd,Ki,qpps)
+    roboclaw.SetM2VelocityPID(BRightAddress,Kp,Kd,Ki,qpps)
 
     # global pub
     # pub = rospy.Publisher('joyDrive', String, queue_size=10)
