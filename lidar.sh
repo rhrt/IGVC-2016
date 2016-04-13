f11b2cbddc78df99507a0870adb64eef4adcd95b
Lewis Kelley <kelleyld@rose-hulman.edu>
2016-04-13 18:03:04 -0400

    Added testing scripts to test reading odometry and localization.

--- lidar.sh
+++ lidar.sh
@@ -2,5 +2,5 @@
 sudo modprobe cdc_acm vendor=0x15d1 product=0x0000
 sudo chmod 777 /dev/ttyACM0
 rosparam set hokuyo_node/calibrate_time false
-rosparam set hokuyo_node/port /dev/ttyACM2
+rosparam set hokuyo_node/port /dev/ttyACM0
 rosparam set hokuyo_node/frame_id /hokuyo_link
